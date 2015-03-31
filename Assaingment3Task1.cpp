c7708833d0309027d638ade1006561aadc10567c
MD. Musfiqur Rahman Sanim <sanim_10@yahoo.com>
2015-03-31 22:08:01 +0600

    Update Assaingment3Task1.cpp

--- Assaingment3Task1.cpp
+++ Assaingment3Task1.cpp
@@ -1,3 +1,8 @@
+/*
+S:- S1 | S2
+S1 :- 0S11 | Ɛ
+S2 :- 1S20 | Ɛ
+*/
 #include<bits/stdc++.h>
 using namespace std;
 const int mx = 1e5 + 7;
@@ -21,6 +26,43 @@ bool check(){
     }
 
     return  !(len%2);
+}
+
+int main(){
+    gets(ch);
+
+    if(check()) cout << "Accepted" << endl;
+    else cout << "Not Accepted" << endl;
+
+    return 0;
+}
+
+----------------------------------------------------------------------
+/*
+S:- aTb | bTa
+ T :- XTX | X | Ɛ
+ X :- a | b
+*/
+#include<bits/stdc++.h>
+using namespace std;
+const int mx = 1e5 + 7;
+char ch[mx];
+
+bool check(){
+    int len = strlen(ch);
+    if(ch[0] == 'a' && ch[len-1] == 'b'){
+        for(int i = 0;i<len;i++){
+            if(ch[i] == 'a' || ch[i] == 'b') continue;
+            return false;
+        }
+    }else if(ch[0] == 'b' && ch[len-1] == 'a'){
+        for(int i = 0;i<len;i++){
+            if(ch[i] == 'a' || ch[i] == 'b') continue;
+            return false;
+        }
+    }else return false;
+
+    return true;
 }
 
 int main(){
